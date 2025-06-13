41f5991ab84e2e095c314da50cf6fa0e36c8fc4a
lizxSJTU <102231087+lizxSJTU@users.noreply.github.com>
2025-06-13 16:02:48 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="DynaRNA",
+    packages=[
+        'data', 'model'
+    ],
+    package_dir={
+        'data': './data',
+        'model': './model',
+          },
+)
